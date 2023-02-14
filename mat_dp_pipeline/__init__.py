e31e84e704c1955d5d8fcab1be7854d1f6572691
Marcin Krupowicz <ma@rcin.me>
2023-02-14 14:34:14 +0000

    Initial commit of the example jupyter notebook

--- mat_dp_pipeline/__init__.py
+++ mat_dp_pipeline/__init__.py
@@ -1,3 +1,16 @@
-from mat_dp_pipeline.pipeline import DataSource, LabelledOutput, pipeline
+__all__ = [
+    "LabelledOutput",
+    "pipeline",
+    "DataSource",
+    "PipelineOutput",
+    "StandardDataFormat",
+    "Year",
+]
 
-__all__ = ["LabelledOutput", "pipeline", "DataSource"]
+from mat_dp_pipeline.pipeline import (
+    DataSource,
+    LabelledOutput,
+    PipelineOutput,
+    pipeline,
+)
+from mat_dp_pipeline.standard_data_format import StandardDataFormat, Year
