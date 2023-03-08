e4c2089a40c962095a7bc4ddd4ede094f66aee5d
Marcin Krupowicz <ma@rcin.me>
2023-03-08 09:32:34 +0000

    Moved App into the sources

--- mat_dp_pipeline/__init__.py
+++ mat_dp_pipeline/__init__.py
@@ -5,8 +5,10 @@ __all__ = [
     "PipelineOutput",
     "StandardDataFormat",
     "Year",
+    "App",
 ]
 
+from mat_dp_pipeline.application import App
 from mat_dp_pipeline.pipeline import (
     DataSource,
     LabelledOutput,
